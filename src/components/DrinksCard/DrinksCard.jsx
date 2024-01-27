73da943ca78bc2845f5d8e66728837db9eef3463
Paris Vigay <parisvigay@pariss-mbp.lan>
2024-01-27 14:22:17 +0000

    Drinks list page done. Just need to sort out the path that the drinks cards link to and then work on the drinks detail page. As of Sat 27/01 afternoon

--- src/components/DrinksCard/DrinksCard.jsx
+++ src/components/DrinksCard/DrinksCard.jsx
@@ -1,10 +1,19 @@
 import './DrinksCard.css'
-import Card from 'react-bootstrap/Card';
 
-export default function DrinksCard() {
+export default function DrinksCard({beer}) {
+  const style = {
+    backgroundImage: `url(${beer.image_url})`,
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat"
+  };
+
   return (
-    <Card id="drinksCard">
-
-    </Card>
+    <div className="drinksCard" style={style}>
+      <p className="cardTitle">{beer.name}</p>
+      <div className="titleCard">
+        <p className="cardDesc">{beer.description}</p>
+      </div>
+    </div>
   )
 }
--- src/pages/DrinksList/DrinksList.jsx
+++ src/pages/DrinksList/DrinksList.jsx
@@ -1,8 +1,33 @@
+import { useState, useEffect } from 'react';
 import DrinksCard from '../../components/DrinksCard/DrinksCard';
 import './DrinksList.css'
 
 export default function DrinksList() {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    async function getBeers() {
+      try {
+        const response = await fetch("https://api.punkapi.com/v2/beers?per_page=10")
+        const beers = await response.json();
+        console.log(beers);
+        setBeers(beers);
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+
+    getBeers();
+  }, []);
+
   return (
-    <DrinksCard />
+    <div className="drinksListMain">
+      <div className="drinksContainer">
+        {beers.map((beer, index) => (
+          <DrinksCard key={index} beer={beer}/>
+        ))}
+      </div>
+    </div>
   )
 }
